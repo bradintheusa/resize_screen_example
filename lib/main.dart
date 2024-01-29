39485e8527fb52d77d5f5a015820879656d282e2
BradInTheUSA <bradintheusa@gmail.com>
2024-01-29 18:39:53 -0500

    Examples added

--- lib/main.dart
+++ lib/main.dart
@@ -38,65 +38,73 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: [
-                Container(color: Colors.amber, child: const Text("One")),
-                Expanded(
-                    child: Container(
-                        color: Colors.cyanAccent, child: const Text("Two"))),
-              ],
-            ),
-            Row(
-              // mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Flexible(
-                    flex: 1,
-                    child: Container(
-                        color: const Color.fromARGB(255, 185, 133, 64),
-                        child: const Text("Three"))),
-                Flexible(
-                    flex: 10,
-                    child: Container(
-                        color: Colors.deepOrangeAccent,
-                        child: const Text("Four"))),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                FractionallySizedBox(
-                    widthFactor: 0.75,
-                    child: Container(
-                        color:  Colors.red,
-                        child: const Text("Five"))),
-                FractionallySizedBox(
-                    widthFactor: 0.25,
-                    child: Container(
-                        color: Colors.deepOrangeAccent,
-                        child: const Text("Six"))),
-              ],
-            ),
-            const Text(
-              'Hello',
-            ),
-            Text(
-              'World',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                children: [
+                  Container(color: Colors.amber, child: const Text("One")),
+                  Expanded(
+                      child: Container(
+                          color: Colors.cyanAccent, child: const Text("Two"))),
+                ],
+              ),
+              Row(
+                // mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                          color: const Color.fromARGB(255, 185, 133, 64),
+                          child: const Text("Three"))),
+                  Flexible(
+                      flex: 10,
+                      child: Container(
+                          color: Colors.deepOrangeAccent,
+                          child: const Text("Four"))),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+              
+                  Flexible(
+                      child: FractionallySizedBox(
+                          widthFactor: 0.30,
+                          child:  FittedBox(
+                          child: Container(
+                              color: Colors.red, child: const Text("Five"))))),
+                  Flexible(
+                      child: FractionallySizedBox(
+                          widthFactor: 0.50,
+                          child: Container(
+                              color: Colors.deepOrangeAccent,
+                              child: const FittedBox(
+                          child: Text("Sixty seven"))))),
+                ],
+              ),
+              const Text(
+                'Hello',
+              ),
+              SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Container(
+                      color: Colors.orangeAccent,
+                      child: const FittedBox(
+                          child: Text(
+                        'World',
+                        style: TextStyle(fontSize: 4),
+                      )))),
+            ],
+          ),
+        ));
   }
 }
